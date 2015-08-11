30417089cf8d1d7ab10a23f2672e04c9c0d2cb72
Pere Urbon-Bayes <pere.urbon@elasticsearch.com>
2015-08-11 11:54:56 +0200

    add initial test that integrate with a fake statsd server

---
+++ spec/spec_helper.rb
@@ -0,0 +1,70 @@
+require "logstash/devutils/rspec/spec_helper"
+require "socket"
+
+
+class StatsdServer
+
+  attr_reader :received, :port
+
+  def initialize
+      @sync_lock = Mutex.new
+      @terminated = false
+      @received   = []
+  end
+
+  def register(port)
+    @port   = port
+    @socket = UDPSocket.new
+    @socket.bind("127.0.0.1", port)
+  end
+
+  def run(port)
+    register(port)
+    Thread.new do
+      while(!closed?)
+        metric, _ = @socket.recvfrom(100)
+        append(metric)
+      end
+    end
+    self
+  end
+
+  def append(metric)
+    @sync_lock.synchronize do
+      @received << metric
+    end
+  end
+
+  def close
+    @sync_lock.synchronize do
+      @terminated = true
+    end
+  end
+
+  def closed?
+    @terminated == true
+  end
+
+end
+
+module StatdHelpers
+
+  def random_port
+    rand(2000..10000)
+  end
+
+end
+
+RSpec.configure do |c|
+
+  c.include StatdHelpers
+
+  c.before(:all) do
+    srand(c.seed)
+    @server = StatsdServer.new.run(rand(2000..10000))
+  end
+
+  c.after(:all) do
+    @server.close
+  end
+end
