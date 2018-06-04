e67a2995ffa1868dcc17ef8686229ff4cde06af4
grahamhar <grahamhar@users.noreply.github.com>
2018-06-04 21:38:16 +0100

    DRY the metrics tests

--- spec/outputs/statsd_spec.rb
+++ spec/outputs/statsd_spec.rb
@@ -12,7 +12,36 @@ describe LogStash::Outputs::Statsd do
     server.close
   end
 
+  shared_examples "metrics" do
+    let(:config) do
+      { "host" => host, "sender" => "spec", "port" => port, "protocol" => protocol, "count" => [ "foo.bar", "0.1" ] }
+    end
+  
+    let(:properties) do
+      { "metric" => "foo.bar", "count" => 10 }
+    end
+  
+    let(:event) { LogStash::Event.new(properties) }
+  
+    subject { LogStash::Outputs::Statsd.new(config) }
+  
+    before(:each) do
+      subject.register
+    end
+  
+    it "should receive data send to the server" do
+      subject.receive(event)
+      # Since we are dealing with threads and networks, 
+      # we might experience delays or timing issues.
+      # lets try a few times before giving up completely.
+      try {
+        expect(server.received).to include("logstash.spec.foo.bar:0.1|c")
+      }
+    end
+  end
+
   describe "UDP" do 
+    let(:protocol) { "udp" }
     let!(:server) { StatsdServer.new.run(port) }
     describe "registration and close" do
 
@@ -22,70 +51,17 @@ describe LogStash::Outputs::Statsd do
       end
     end
 
-
-    describe "#send" do
-
-      context "count metrics" do
-
-        let(:config) do
-          { "host" => host, "sender" => "spec", "port" => port, "count" => [ "foo.bar", "0.1" ] }
-        end
-
-        let(:properties) do
-          { "metric" => "foo.bar", "count" => 10 }
-        end
-
-        let(:event) { LogStash::Event.new(properties) }
-
-        subject { LogStash::Outputs::Statsd.new(config) }
-
-        before(:each) do
-          subject.register
-        end
-
-        it "should receive data send to the server" do
-          subject.receive(event)
-          # Since we are dealing with threads and networks, 
-          # we might experience delays or timing issues.
-          # lets try a few times before giving up completely.
-          try {
-            expect(server.received).to include("logstash.spec.foo.bar:0.1|c")
-          }
-        end
-      end
+    context "#send" do
+      include_examples "metrics"
     end
+
   end
 
   describe "#send TCP" do
-
+    let(:protocol) { "tcp" }
     let!(:server) { StatsdServer.new.run(port, "tcp") }
-    context "count metrics" do
-
-      let(:config) do
-        { "host" => host, "sender" => "spec", "port" => port, "protocol" => "tcp", "count" => [ "foo.bar", "0.1" ] }
-      end
-
-      let(:properties) do
-        { "metric" => "foo.bar", "count" => 10 }
-      end
-
-      let(:event) { LogStash::Event.new(properties) }
-
-      subject { LogStash::Outputs::Statsd.new(config) }
-
-      before(:each) do
-        subject.register
-      end
-
-      it "should receive data send to the TCP server" do
-        subject.receive(event)
-        # Since we are dealing with threads and networks,
-        # we might experience delays or timing issues.
-        # lets try a few times before giving up completely.
-        try {
-          expect(server.received).to include("logstash.spec.foo.bar:0.1|c")
-        }
-      end
+    context "#send metrics" do
+      include_examples "metrics"
     end
   end
 end
